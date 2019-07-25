0c9d89edd69011300ca7c51c6eee3e8fcd6e486b
Joel Worrall <jworrall@newrelic.com>
2019-07-24 17:34:31 -0700

    initial checkin.

---
+++ lab9/nerdlets/my-nerdlet/add-entity-dialog.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { EntitySearchQuery, Dialog, Stack, StackItem, TextField, Spinner } from 'nr1';
+import { alertSeverityToColor, decodeEntityFromEntityId } from './utils';
+
+export default class AddEntityDialog extends Component {
+  static propTypes = {
+    entities: PropTypes.arrayOf(PropTypes.object),
+    entityType: PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      domain: PropTypes.string.isRequired
+    }),
+    onSearchSelect: PropTypes.func,
+    onClose: PropTypes.func,
+    openDialog: PropTypes.bool
+  };
+
+  static defaultProps = {
+    entityType: { type: 'APPLICATION', domain: "APM" }
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: null,
+      isLoading: false,
+      openDialog: this.props.openDialog
+    };
+    this.onClick = this.onClick.bind(this);
+    this.onSearch = this.onSearch.bind(this);
+  }
+
+  componentWillUpdate(nextProps) {
+    if (this.state.openDialog != nextProps.openDialog) {
+      this.setState({ openDialog: nextProps.openDialog });
+    }
+  }
+
+  onSearch(e) {
+    const query = e.target.value;
+    //console.debug("onSearch", query);
+    if (query && query.length > 3) {
+      const { entityType } = this.props;
+      this.setState({ isLoading: true });
+      const filters = [{
+        type: 'searchQuery',
+        value: query
+      },
+      {
+        type: 'entityType',
+        value: { type: entityType.type, domain: entityType.domain }
+      }];
+      const { entities } = this.props;
+      if (entities) {
+        const entity = decodeEntityFromEntityId(entities[0].id);
+        filters.push({
+          type:'tag',
+          value: { key: 'accountId', value: entity.accountId }
+        });
+      }
+      EntitySearchQuery.query({ filters }).then(rs => {
+        //console.log("onSearch results", rs);
+        if (rs.data) {
+          const results = rs.data.actor.entitySearch.results.entities.map(entity => {
+            //console.debug("Checking", entity);
+            if (!entities || !entities.find(ex => ex.id = entity.id)) {
+              return entity;
+            }
+          });
+          //console.debug("Found the following in the search", results);
+          this.setState({ isLoading: false, results });
+        } else {
+          this.setState({ isLoading: false });
+        }
+      }).catch(error => {
+          console.error(error); //eslint-disable-line
+      });
+    }
+  }
+
+  onClick(entity) {
+    this.props.onSearchSelect(entity);
+  }
+
+  render() {
+    const { results, isLoading, openDialog } = this.state;
+    const { entityType } = this.props;
+    const label = entityType.domain == 'BROWSER' ? 'Browser Apps' : 'APM Services';
+    //console.log(results);
+    return (
+      <Dialog hidden={!openDialog} onClose={(...args) => {
+          this.setState({ openDialog: false });
+          //console.debug(args);
+        }}>
+        <Stack directionType={Stack.DIRECTION_TYPE.VERTICAL} distributionType="fill">
+          <StackItem>
+            <TextField
+              label={`Search ${label}`}
+              type="search"
+              onChange={this.onSearch}
+            />
+          </StackItem>
+          <StackItem>
+            {isLoading ?
+              <Spinner className="centered" /> :
+              <div>
+                {results && <p>{results.length} {results.length == 1 ? 'result' : 'results'}...</p>}
+                <ul className="resultSet">
+                  {results ? results.map((entity, i) => {
+                    return <li key={i} onClick={() => { this.onClick(entity); }} style={{borderLeft: `4px solid ${alertSeverityToColor(entity)}` }}>{entity.name}</li>
+                  }) : <li>
+                    No results available.
+                  </li>}
+                </ul>
+              </div>
+            }
+          </StackItem>
+        </Stack>
+      </Dialog>
+    )
+  }
+}
---
+++ lab9/nerdlets/my-nerdlet/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { LineChart, TableChart, Grid, GridItem, Spinner, DisplayText, Button, Icon } from 'nr1';
+import { loadEntity, decodeEntityFromEntityId, distanceOfTimeInWords } from './utils';
+import AddEntityDialog from './add-entity-dialog';
+
+export default class MyNerdlet extends React.Component {
+    static propTypes = {
+        width: PropTypes.number,
+        height: PropTypes.number.isRequired,
+        launcherUrlState: PropTypes.object.isRequired,
+        nerdletUrlState: PropTypes.object.isRequired
+    }
+
+    constructor(props) {
+        super(props);
+        //console for learning purposes
+        console.debug(props); //eslint-disable-line
+        //initiate the state
+        this.state = {
+            entities: [],
+            entityType: { domain: 'APM', type: 'APPLICATION'},
+            openDialog: false
+        }
+        this.onSearchSelect = this.onSearchSelect.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.props.nerdletUrlState && this.props.nerdletUrlState.entityId) {
+            loadEntity(this.props.nerdletUrlState.entityId).then(entity => {
+                this.setState({ entityType: {domain: entity.domain, type: entity.type}, entities: [ entity ]});
+            });
+        }
+    }
+
+    componentWillUpdate(nextProps) {
+        if (this.props && nextProps.nerdletUrlState && nextProps.nerdletUrlState.entityId != this.props.nerdletUrlState.entityId) {
+            loadEntity(this.props.nerdletUrlState.entityId).then(entity => {
+                this.setState({ entityType: {domain: entity.domain, type: entity.type}, entities: [ entity ]});
+            });
+        }
+        return true;
+    }
+
+    /**
+     * Receive an entity from the EntitySearch
+     * @param {Object} entity
+     */
+    onSearchSelect(entity) {
+        const { entities } = this.state;
+        entities.push(entity);
+        this.setState({ entities });
+    }
+
+    _buildNrql(base) {
+        const { entities } = this.state;
+        const appNames = entities ? entities.map((entity, i) => `'${entity.name}'`) : null;
+        let nrql = `${base} FACET appName ${appNames ? `WHERE appName in (${appNames.join(",")}) ` : ''}`;
+        return nrql;
+    }
+
+    render() {
+        const { height, launcherUrlState, nerdletUrlState } = this.props;
+        if (!nerdletUrlState || !nerdletUrlState.entityId) {
+            return <AddEntityDialog
+                     onSearchSelect={this.onSearchSelect}
+                   />;
+        } else {
+            //entityId is four-item array of accountId|domain|type|id
+            const { entities, openDialog } = this.state;
+            const entity = decodeEntityFromEntityId(nerdletUrlState.entityId);
+            const { accountId } = entity;
+            const eventType = entity ? entity.domain == 'BROWSER' ? 'PageView' : 'Transaction' : null;
+            const { timeRange : { duration }} = launcherUrlState;
+            const durationInMinutes = duration / 1000 / 60;
+            const label = entityType.domain == 'BROWSER' ? 'Browser Apps' : 'APM Services';
+            return (<React.Fragment><Grid>
+                    {entities && entities.length > 0 ? <React.Fragment><GridItem columnStart={1} columnEnd={12} style={{padding: '10px'}}>
+                        <DisplayText>Performance over Time<Button sizeType={Button.SIZE_TYPE.SLIM} style={{marginLeft: '25px'}} onClick={() => { this.setState({ openDialog: true }) }}><Icon name="interface_sign_plus" /> {label}</Button></DisplayText>
+                        <p style={{marginBottom: '10px'}}>{distanceOfTimeInWords(duration)}</p>
+                        <LineChart
+                            accountId={accountId}
+                            query={this._buildNrql(`SELECT average(duration) from ${eventType} TIMESERIES SINCE ${durationInMinutes} MINUTES AGO `)}
+                            style={{height: `${height*.5}px`}}
+                        />
+                    </GridItem>
+                    <GridItem columnStart={1} columnEnd={12}>
+                        <TableChart
+                            accountId={accountId}
+                            query={this._buildNrql(`SELECT count(*) as 'requests', percentile(duration, 99, 90, 50) FROM ${eventType} SINCE ${durationInMinutes} MINUTES AGO`)}
+                            style={{height: `${height*.5}px`}}
+                        />
+                    </GridItem>
+                    </React.Fragment> : <Spinner className="centered" />}
+                    {openDialog && <AddEntityDialog
+                        openDialog={openDialog}
+                        entities={entities}
+                        onSearchSelect={this.onSearchSelect}
+                    />}
+                </Grid>
+            </React.Fragment>);
+        }
+
+    }
+}
