093fd17c1708344e9808072f9b37f91bb3c66a13
Joel Worrall <jworrall@newrelic.com>
2019-09-16 06:41:41 -0400

    fix(Lab 7): Use AutoSizer.

--- lab7/nerdlets/my-nerdlet/index.js
+++ lab7/nerdlets/my-nerdlet/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Grid, GridItem } from 'nr1';
+import { Grid, GridItem, AutoSizer } from 'nr1';
 
 export default class MyNerdlet extends React.Component {
-    static propTypes = {
-        width: PropTypes.number,
-        height: PropTypes.number,
-        launcherUrlState: PropTypes.object,
-        nerdletUrlState: PropTypes.object
-    };
+
 
     constructor(props) {
         super(props);
@@ -16,78 +10,80 @@ export default class MyNerdlet extends React.Component {
     }
 
     render() {
-        const { height } = this.props;
         //we're going to replace these in the lab INSTRUCTIONS.md, so watch while you can.
-        return <Grid>
-            <GridItem columnStart={1} columnEnd={12}>
-                <h1>Here's a little fun with Grid...</h1>
-            </GridItem>
-            <GridItem columnStart={1} columnEnd={6}>
-                <iframe width="100%" height={height/3} src="https://www.youtube.com/embed/caIsN_PjaHY" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={7} columnEnd={12}>
-                <iframe width="100%" height={height/3} src="https://www.youtube.com/embed/9S1EzkRpelY" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={1} columnEnd={1}>
-                <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={2} columnEnd={2}>
-                <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={3} columnEnd={3}>
-                <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={4} columnEnd={4}>
-                <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={5} columnEnd={5}>
-                <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={6} columnEnd={11}>
-                <h2>The point is, it's really flexible.</h2>
-            </GridItem>
-            <GridItem columnStart={12} columnEnd={12}>
-                <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={1} columnEnd={2}>
-                <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={3} columnEnd={4}>
-                <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={5} columnEnd={6}>
-                <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={7} columnEnd={8}>
-                <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={9} columnEnd={10}>
-                <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={11} columnEnd={12}>
-                <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={1} columnEnd={4}>
-                <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={5} columnEnd={8}>
-                <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={9} columnEnd={12}>
-                <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={1} columnEnd={3}>
-                <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={4} columnEnd={6}>
-                <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={7} columnEnd={9}>
-                <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-            <GridItem columnStart={10} columnEnd={12}>
-                <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-            </GridItem>
-        </Grid>;
+        return <AutoSizer>
+            {({ height }) => (<Grid>
+                <GridItem columnStart={1} columnEnd={12}>
+                    <h1>Here's a little fun with Grid...</h1>
+                </GridItem>
+                <GridItem columnStart={1} columnEnd={6}>
+                    <iframe width="100%" height={height/3} src="https://www.youtube.com/embed/caIsN_PjaHY" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={7} columnEnd={12}>
+                    <iframe width="100%" height={height/3} src="https://www.youtube.com/embed/9S1EzkRpelY" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={1} columnEnd={1}>
+                    <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={2} columnEnd={2}>
+                    <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={3} columnEnd={3}>
+                    <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={4} columnEnd={4}>
+                    <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={5} columnEnd={5}>
+                    <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={6} columnEnd={11}>
+                    <h2>The point is, it's really flexible.</h2>
+                </GridItem>
+                <GridItem columnStart={12} columnEnd={12}>
+                    <iframe width="100%" height={height/12} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={1} columnEnd={2}>
+                    <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={3} columnEnd={4}>
+                    <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={5} columnEnd={6}>
+                    <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={7} columnEnd={8}>
+                    <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={9} columnEnd={10}>
+                    <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={11} columnEnd={12}>
+                    <iframe width="100%" height={height/6} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={1} columnEnd={4}>
+                    <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={5} columnEnd={8}>
+                    <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={9} columnEnd={12}>
+                    <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={1} columnEnd={3}>
+                    <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/LTunhRVyREU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={4} columnEnd={6}>
+                    <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={7} columnEnd={9}>
+                    <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/QH2-TGUlwu4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+                <GridItem columnStart={10} columnEnd={12}>
+                    <iframe width="100%" height={height/4} src="https://www.youtube.com/embed/GM-e46xdcUo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                </GridItem>
+            </Grid>
+        )}
+        </AutoSizer>;
     }
 }
