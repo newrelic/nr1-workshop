57cc73da3c414584ac42564ebef247a6119a0197
Alex Ronquillo <aronquillo@newrelic.com>
2021-07-01 10:33:13 -0400

    Update screenshot

--- lab5/nerdlets/my-nerdlet/index.js
+++ lab5/nerdlets/my-nerdlet/index.js
@@ -1,8 +1,9 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
+import { NerdGraphQuery, EntityByGuidQuery, EntitiesByNameQuery, EntitiesByDomainTypeQuery, EntityCountQuery, Spinner, Stack, StackItem, HeadingText, BlockText, NerdletStateContext } from 'nr1';
 
 export default class MyNerdlet extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         console.debug(props); //eslint-disable-line
         this.state = {
@@ -11,6 +12,14 @@ export default class MyNerdlet extends React.Component {
     }
 
     render() {
-        return (<h1>Hello World lab5!</h1>);
+        return (
+            <NerdGraphQuery query={`{actor {user {name email}}}`}>
+                {({ loading, error, data }) => {
+                    console.debug([loading, data, error]); //eslint-disable-line
+                    return null
+                }}
+            </NerdGraphQuery>
+        )
     }
+
 }
